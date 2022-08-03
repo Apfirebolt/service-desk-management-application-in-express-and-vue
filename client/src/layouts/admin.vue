d38e951942184681c90a038c3af33b87106e4a5d
Amit P <aspper20@gmail.com>
2022-08-04 00:44:19 +0530

    feat: added styles for admin and staff view

--- client/src/layouts/admin.vue
+++ client/src/layouts/admin.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <p class="text-3xl text-center py-3 text-red-600">
-      Admin Layout
+    <header-component />
+    <p class="text-3xl w-full bg-gray-100 text-center py-3 text-red-600">
+      ADMIN
     </p>
     <router-view />
+    <footer-component />
   </div>
 </template>
 <script>
+import HeaderComponent from '../components/common/header.vue';
+import FooterComponent from '../components/common/footer.vue';
+
 export default {
   name: 'AdminLayout',
+  components: {
+    HeaderComponent,
+    FooterComponent
+  }
 };
 </script>
--- client/src/layouts/staff.vue
+++ client/src/layouts/staff.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <p class="text-3xl text-center py-3 text-red-600">
-      Staff Layout
+    <header-component />
+    <p class="text-3xl w-full bg-gray-100 text-center py-3 text-red-600">
+      STAFF
     </p>
     <router-view />
+    <footer-component />
   </div>
 </template>
 <script>
+import HeaderComponent from '../components/common/header.vue';
+import FooterComponent from '../components/common/footer.vue';
+
 export default {
   name: 'StaffLayout',
+  components: {
+    HeaderComponent,
+    FooterComponent
+  }
 };
 </script>
