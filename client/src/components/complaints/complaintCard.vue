b89f19b57a6fdca143ad436f5ad70cce4a16421f
Amit P <aspper20@gmail.com>
2022-12-31 03:19:19 +0530

    feat: complaint card style changed

--- client/src/components/complaints/complaintCard.vue
+++ client/src/components/complaints/complaintCard.vue
@@ -1,27 +1,36 @@
 <template>
-  <div class="flex flex-col justify-center">
-    <p class="text-2xl text-center m-2">
-      {{ complaint.title }}
-    </p>
-    <p class="text-center">
-      {{ complaint.description }}
-    </p>
-    <p class="text-center text-lg my-4">
-      Status - <span class="bg-green-300 p-2">{{ complaint.status }}</span>
-    </p>
-    <p v-if="complaint.managedBy" class="text-center text-lg my-4">
-      Managed By - <span class="bg-purple-300 p-2">{{ complaint.managedBy.firstName + ' ' + complaint.managedBy.lastName }}</span>
-    </p>
-    <div class="flex justify-center items-center my-5">
-      <t-button type="button" class="mx-2" variant="error" @click="deleteComplaintUtil">
-        Delete
-      </t-button>
-      <t-button type="button" class="mx-2" @click="updateComplaintUtil">
-        Update
-      </t-button>
+  <div class="flex flex-col bg-white justify-center">
+    <div class="rounded overflow-hidden shadow-lg">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">{{ complaint.title }}</div>
+        <p class="text-gray-700 text-base">
+          {{ complaint.description }}
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span
+          v-if="complaint.managedBy"
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+        >
+         Managed By - {{ complaint.managedBy.firstName + " " + complaint.managedBy.lastName }}
+        </span>
+        <span
+          v-if="complaint.managedBy"
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+        >
+         Status - {{ complaint.status }}
+        </span>
+      </div>
+      <div class="flex justify-center items-center my-5">
+        <t-button type="button" class="mx-2" variant="error" @click="deleteComplaintUtil">
+          Delete
+        </t-button>
+        <t-button type="button" class="mx-2" @click="updateComplaintUtil"> Update </t-button>
+      </div>
     </div>
   </div>
 </template>
+
 <script>
 export default {
   name: "ComplaintCard",
@@ -33,11 +42,11 @@ export default {
   },
   methods: {
     deleteComplaintUtil() {
-      this.$emit('deleteComplaint', this.complaint._id);
+      this.$emit("deleteComplaint", this.complaint._id);
     },
     updateComplaintUtil() {
-      this.$emit('updateComplaint', this.complaint._id);
-    }
-  }
+      this.$emit("updateComplaint", this.complaint._id);
+    },
+  },
 };
 </script>
